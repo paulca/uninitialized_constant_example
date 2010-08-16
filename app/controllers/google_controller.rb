bdc36c5ca8f2382fbdda9c0c9065ae0d885472dc
Paul Campbell <paul@rslw.com>
2010-08-16 18:09:08 +0100

    slight change

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -2,7 +2,6 @@ class GoogleController < ApplicationController
   
   def show
     @size = Size.new
-    render :inline => @size.get_google
   end
   
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 class Size < ActiveRecord::Base
   
   def get_google
-    open('http://google.com')
+    open('http://google.com').read
   end
   
 end
